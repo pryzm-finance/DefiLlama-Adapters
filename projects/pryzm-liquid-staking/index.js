b7b27269b34245bcc3bb63434a6a5d1fab7355b0
Mehrdad <mehrdadpfg@live.com>
2025-07-08 13:36:07 +0200

    feat: add pryzm token balance

--- projects/pryzm-liquid-staking/index.js
+++ projects/pryzm-liquid-staking/index.js
@@ -7,47 +7,57 @@ const host_chains = {
     coinGeckoId: "cosmos",
     decimals: 1e6,
   },
-
   injective: {
     hostChainId: "inj",
     coinGeckoId: "injective-protocol",
     decimals: 1e18,
   },
-
   osmosis: {
     hostChainId: "uosmo",
     coinGeckoId: "osmosis",
     decimals: 1e6,
   },
-
   terra2: {
     hostChainId: "uluna",
     coinGeckoId: "terra-luna-2",
     decimals: 1e6,
   },
-
   celestia: {
     hostChainId: "utia",
     coinGeckoId: "celestia",
+    decimals: 1e6,
+  },
+  pryzm: {
+    hostChainId: "upryzm",
+    coinGeckoId: "pryzm",
     decimals: 1e6,
   },
 };
 
 const endpoint = endPoints["pryzm"]
 
-function tvlOnChain(chain) {
+function tvlOnChain(chainName, chain) {
   return async (api) => {
-    const [{ amount: coin }, { host_chain_state: state }] =
-      await Promise.all([
-        await get(`${endpoint}/cosmos/bank/v1beta1/supply/by_denom?denom=c:${chain.hostChainId}`),
-        await get(`${endpoint}/pryzm/icstaking/v1/host_chain_state/${chain.hostChainId}`),
-      ]);
+    if (chainName === "pryzm") {
+      const data = await get("https://pryzmatics.pryzm.zone/pryzmatics/token/upryzm?detailed_stats=true");
+      const price = Number(data?.token?.price || 0);
+      const supply = Number(data?.token?.supply || 0);
+      const pryzmBalance = price * (supply / chain.decimals);
 
-    const balance = coin.amount * state.exchange_rate / chain.decimals
-    api.addCGToken(chain.coinGeckoId, balance)
+      api.addCGToken(chain.coinGeckoId, pryzmBalance);
+    } else {
+      const [{ amount: coin }, { host_chain_state: state }] =
+        await Promise.all([
+          get(`${endpoint}/cosmos/bank/v1beta1/supply/by_denom?denom=c:${chain.hostChainId}`),
+          get(`${endpoint}/pryzm/icstaking/v1/host_chain_state/${chain.hostChainId}`),
+        ]);
+
+      const balance = coin.amount * state.exchange_rate / chain.decimals;
+      api.addCGToken(chain.coinGeckoId, balance);
+    }
   };
 }
 
 for (const chainName of Object.keys(host_chains)) {
-  module.exports[chainName] = { tvl: tvlOnChain(host_chains[chainName]) };
+  module.exports[chainName] = { tvl: tvlOnChain(chainName, host_chains[chainName]) };
 }
